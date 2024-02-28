1059b3d55b3d6e353b80f53c07feb8bbdc14121a
SAIKIRANPATNANA <saikiranpatnana5143@gmail.com>
2024-02-28 09:30:04 +0530

    updated

--- main.py
+++ main.py
@@ -40,7 +40,9 @@ elif st.toggle('Record Audio'):
         recorded_file = 'tested_recordings/recorded_audio.wav'
         if predict(recorded_file)==1:
             st.success("Your recorded audio is of a Male.")
+            exit()
         else:
             st.success("Your recorded audio is of a Female.")
+            exit()
 
 # model size: 0.5 mb
